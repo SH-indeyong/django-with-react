4f95f46eb2f013cb1f7edf4967ea12dd8bd70971
SH-indeyong <ohsh1115@gmail.com>
2023-08-03 22:18:06 +0900

    signup

--- askcompany/settings/common.py
+++ askcompany/settings/common.py
@@ -41,8 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Third Apps
+    'bootstrap4',
     'debug_toolbar',
     # Local Apps
+    'accounts',
 
 ]
 
@@ -90,6 +92,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
