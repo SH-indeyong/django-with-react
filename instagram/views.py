ca207c2f28c35e1d3d1bac5298d6391bb98d7376
SH-indeyong <ohsh1115@gmail.com>
2023-08-04 19:34:54 +0900

    follow/unfollow

--- instagram/views.py
+++ instagram/views.py
@@ -5,11 +5,16 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostForm
 from .models import Tag, Post
 
+
 @login_required
 def index(request):
-    return render(request, 'instagram/index.html', {
-        
+    suggested_user_list = get_user_model().objects.all()\
+        .exclude(pk=request.user.pk)\
+        .exclude(pk__in=request.user.following_set.all())
+    return render(request, "instagram/index.html", {
+        'suggested_user_list': suggested_user_list,
     })
+
 
 @login_required
 def post_new(request):
@@ -19,27 +24,41 @@ def post_new(request):
             post = form.save(commit=False)
             post.author = request.user
             post.save()
-            post.tag_set.add(*post.extract_tag_list())          
-            messages.success(request, '포스팅을 저장했습니다.')
-            return redirect(post)    # TODO: get_absolute_url 활용
+            post.tag_set.add(*post.extract_tag_list())
+            messages.success(request, "포스팅을 저장했습니다.")
+            return redirect(post)  # TODO: get_absolute_url 활용
     else:
         form = PostForm()
-    return render(request, 'instagram/post_form.html', {
-      'form': form,  
-    })
+    return render(
+        request,
+        "instagram/post_form.html",
+        {
+            "form": form,
+        },
+    )
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'instagram/post_detail.html', {
-        'post': post,
-    })
+    return render(
+        request,
+        "instagram/post_detail.html",
+        {
+            "post": post,
+        },
+    )
+
 
 def user_page(request, username):
     page_user = get_object_or_404(get_user_model(), username=username, is_active=True)
     post_list = Post.objects.filter(author=page_user)
     post_list_count = post_list.count()
-    return render(request, 'instagram/user_page.html', {
-        'page_user': page_user,
-        'post_list': post_list,
-        'post_list_count': post_list_count,
-    })
+    return render(
+        request,
+        "instagram/user_page.html",
+        {
+            "page_user": page_user,
+            "post_list": post_list,
+            "post_list_count": post_list_count,
+        },
+    )
