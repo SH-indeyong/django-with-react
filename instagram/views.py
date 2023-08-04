38ba19cd5d3e64bcb0990e09e28e88f73dbe4c72
SH-indeyong <ohsh1115@gmail.com>
2023-08-04 23:30:13 +0900

    comment

--- instagram/views.py
+++ instagram/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 from .models import Tag, Post
 
 
@@ -21,12 +21,15 @@ def index(request):
             created_at__gte=timesince
         )
     
+    comment_form = CommentForm()
+    
     suggested_user_list = get_user_model().objects.all()\
         .exclude(pk=request.user.pk)\
         .exclude(pk__in=request.user.following_set.all())
     return render(request, "instagram/index.html", {
         'suggested_user_list': suggested_user_list,
         'post_list': post_list,
+        'comment_form': comment_form,
     })
 
 
@@ -54,11 +57,13 @@ def post_new(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    comment_form = CommentForm()
     return render(
         request,
         "instagram/post_detail.html",
         {
             "post": post,
+            'comment_form': comment_form,
         },
     )
 
@@ -77,6 +82,24 @@ def post_unlike(request, pk):
     messages.success(request, f"포스팅 #{post.pk} 좋아요를 취소합니다.")
     redirect_url = request.META.get("HTTP_REFERER", "root")
     return redirect(redirect_url)
+
+@login_required
+def comment_new(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST, request.FILES)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect(comment.post)
+    else:
+        form = CommentForm()
+    return render(request, 'instagram/comment_form.html', {
+        'form': form,
+    })
 
 def user_page(request, username):
     page_user = get_object_or_404(get_user_model(), username=username, is_active=True)
